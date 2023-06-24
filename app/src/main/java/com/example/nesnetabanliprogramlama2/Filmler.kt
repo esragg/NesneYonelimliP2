aed0a72f0f479f836ba7eec83f152dbacff4628a
Esra GARCIA GORMAN <esragarcia@hotmail.com>
2023-06-24 09:53:30 +0200

    Nesne-Yonelimli-Programlama 2. kisim eklenti

---
+++ app/src/main/java/com/example/nesnetabanliprogramlama2/Filmler.kt
@@ -0,0 +1,9 @@
+package com.example.nesnetabanliprogramlama2
+
+class Filmler(var film_id:Int,
+              var  film_ad:String,
+              var film_yil:Int,
+              var kategori:Kategoriler,
+              var yonetmen:Yonetmenler) {
+
+}
---
+++ app/src/main/java/com/example/nesnetabanliprogramlama2/Kategoriler.kt
@@ -0,0 +1,4 @@
+package com.example.nesnetabanliprogramlama2
+
+class Kategoriler(var kategori_id:Int, var kategori_ad:String) {
+}
